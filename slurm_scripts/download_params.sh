4ba36a59b927eaeddc831bab44827cdb063ca9f1
Starksplosh4 <109884686+Starksplosh4@users.noreply.github.com>
2024-05-06 00:22:00 -0400

    Update download_params.sh

--- slurm_scripts/download_params.sh
+++ slurm_scripts/download_params.sh
@@ -4,6 +4,6 @@
 #SBATCH --mem-per-cpu=10G
 
 # Move to the directory where we want the params folder to go
-cd /home/cpsc471_ljm78/final_project
+cd ..
 
-srun python3 /home/cpsc471_ljm78/final_project/python_scripts/alphafold_download_params.py
+srun python3 ../python_scripts/alphafold_download_params.py
