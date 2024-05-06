9bde5959eb10645d52a378906ffde904e4271c31
Starksplosh4 <109884686+Starksplosh4@users.noreply.github.com>
2024-05-06 14:26:39 -0400

    Update run_monomer_perturb.sh

--- slurm_scripts/run_monomer_perturb.sh
+++ slurm_scripts/run_monomer_perturb.sh
@@ -6,12 +6,12 @@
 
 # Ensure that --array has the correct number of jobs
 
-config=/home/cpsc471_ljm78/final_project/job_array_config_files/monomer_config.txt
+config = ../config_files/monomer_config.txt
 
 seq=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 delete_index=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
 
 # Move to the directory where we want the output to go
-cd /home/cpsc471_ljm78/final_project/garbage_out
+cd ../monomer_garbage_out
 
-srun python3 /home/cpsc471_ljm78/final_project/python_scripts/alphafold_monomer.py ${seq} ${delete_index}
+srun python3 ../python_scripts/alphafold_monomer.py ${seq} ${delete_index}
