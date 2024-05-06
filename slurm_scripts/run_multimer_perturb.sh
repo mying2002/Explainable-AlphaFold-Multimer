fff5597bfe5afcef3e4757fc688edf83a034ee9f
Starksplosh4 <109884686+Starksplosh4@users.noreply.github.com>
2024-05-06 14:27:03 -0400

    Update run_multimer_perturb.sh

--- slurm_scripts/run_multimer_perturb.sh
+++ slurm_scripts/run_multimer_perturb.sh
@@ -6,7 +6,7 @@
 
 # Ensure that --array has the correct number of jobs
 
-config=/home/cpsc471_ljm78/final_project/job_array_config_files/multimer_config.txt
+config = ../config_files/multimer_config.txt
 
 seq1_id=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 seq2_id=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
@@ -16,6 +16,6 @@ which_seq=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $6}'
 delete_index=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $7}' $config)
 
 # Move to the directory where we want the output to go
-cd /home/cpsc471_ljm78/final_project/garbage_out
+cd ../multimer_garbage_out
 
-srun python3 /home/cpsc471_ljm78/final_project/python_scripts/alphafold_multimer.py ${seq1_id} ${seq2_id} ${seq1} ${seq2} ${which_seq} ${delete_index}
+srun python3 ../python_scripts/alphafold_multimer.py ${seq1_id} ${seq2_id} ${seq1} ${seq2} ${which_seq} ${delete_index}
